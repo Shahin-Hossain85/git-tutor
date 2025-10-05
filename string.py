c75a1bf03d73084c4984e229130008dfe6d8c033
Md Shahin Hossain <shahin.ltx@gmail.com>
2025-10-05 22:42:39 +0600

    Create string file

---
+++ string.py
@@ -0,0 +1,16 @@
+# string indexing
+'''
+text = "Hello World" # countin will start 0
+print(text[0])
+print(text[6])
+print(text[6])
+
+'''
+# Negative indexing
+
+'''
+text = "Hello World" # countin will start 0
+print(text[-1])
+print(text[-5])
+
+'''
